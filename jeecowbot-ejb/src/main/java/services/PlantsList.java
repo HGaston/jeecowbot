42a0cd0b03fb7738c14b8b01e65111486e3e5907
Ghassen <Ghassen@PC-Gaston>
2017-02-21 13:47:01 +0100

    second
    Signed-off-by: Ghassen <Ghassen@PC-Gaston>

---
+++ jeecowbot-ejb/src/main/java/services/PlantsList.java
@@ -0,0 +1,18 @@
+package services;
+
+import javax.ejb.Singleton;
+
+/**
+ * Session Bean implementation class PlantsList
+ */
+@Singleton
+public class PlantsList implements PlantsListRemote, PlantsListLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public PlantsList() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
