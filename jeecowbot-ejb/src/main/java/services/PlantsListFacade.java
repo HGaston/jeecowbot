40e0eaf28a091f32bf90613cd38486b9115f5755
sami <sami.marrekchi@esprit.tn>
2017-02-21 13:50:43 +0100

    FacadPlantsList
    Signed-off-by: sami <sami.marrekchi@esprit.tn>

---
+++ jeecowbot-ejb/src/main/java/services/PlantsListFacade.java
@@ -0,0 +1,18 @@
+package services;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class PlantsListFacade
+ */
+@Stateless
+public class PlantsListFacade implements PlantsListFacadeRemote, PlantsListFacadeLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public PlantsListFacade() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
